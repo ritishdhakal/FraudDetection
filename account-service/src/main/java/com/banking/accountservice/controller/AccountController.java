c064faea6eac6774c6b00567e294f0c31632cbf5
ritishdhakal <dhakalritish@gmail.com>
2026-09-12 04:37:12 +1000

    Added dto and controller

---
+++ account-service/src/main/java/com/banking/accountservice/controller/AccountController.java
@@ -0,0 +1,106 @@
+package com.banking.accountservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banking.accountservice.dto.AccountResponse;
+import com.banking.accountservice.dto.CreateAccountRequest;
+import com.banking.accountservice.service.AccountService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+@Slf4j
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    // create an account
+    // when user create account we want to ask few things and that things are
+    // written int DTO
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(
+            @Valid @RequestBody CreateAccountRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(accountService.createAccount(request));
+    }
+
+    // get account Number
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<AccountResponse> getAccount(
+            @PathVariable String accountNumber //// takes the value directly from the url (GET /accounts/ACC123) then
+                                               //// accountNumber = ACC123
+    ) {
+        return ResponseEntity.ok(accountService.getAccount(accountNumber));
+    }
+
+    // get balance (We need accountNUmebr to get the balance)
+
+    @GetMapping("/{accountNumber}/balance")
+    public ResponseEntity<BigDecimal> getBalance(
+            @PathVariable String accountNumber) {
+        return ResponseEntity.ok(accountService.getBalance(accountNumber));
+    }
+
+    @PutMapping("/{accountNumber}/block")
+    public ResponseEntity<String> blockAccount(
+            @PathVariable String accountNumber) {
+        accountService.blockAccount(accountNumber);
+        return ResponseEntity.ok("Account blocked successfully");
+    }
+
+    /*
+     * SAGA Step 1: Debit sender's account
+     *
+     * Called by the Transaction Service when a transfer is approved.
+     *
+     * The Account Service deducts the transfer amount from the sender's balance.
+     */
+
+    @PutMapping("/{accountNumber}/deduct")
+    public ResponseEntity<String> deductBalance(
+            @PathVariable String accountNumber,
+            @RequestParam BigDecimal amount) {
+        accountService.deductBalance(accountNumber, amount);
+        return ResponseEntity.ok("Balance deducted successfully");
+    }
+
+    @PutMapping("/{accountNumber}/credit")
+    public ResponseEntity<String> creditBalance(
+            @PathVariable String accountNumber,
+            @RequestParam BigDecimal amount) {
+        accountService.creditBalance(accountNumber, amount);
+        return ResponseEntity.ok("Balance deducted successfully");
+    }
+
+    /*
+     * SAGA Step 2: Credit account / Compensation
+     *
+     * Called by the Transaction Service in two scenarios:
+     *
+     * 1. Transaction completed:
+     * Credit the transfer amount to the receiver's account.
+     *
+     * 2. Compensation:
+     * If a later Saga step fails, refund the sender by crediting
+     * the deducted amount back to their account.
+     */
+
+}
---
+++ account-service/src/main/java/com/banking/accountservice/dto/CreateAccountRequest.java
@@ -0,0 +1,35 @@
+package com.banking.accountservice.dto;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateAccountRequest {
+
+    @NotBlank(message = "Name is required")
+    private String accountHolderName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Format not matched")
+    private String email;
+
+    @NotBlank(message = "Phonen number is required")
+    private String phone;
+
+    @NotBlank(message = "Account type is required")
+
+    private String accountType;
+
+    @NotBlank(message = "Initial deposit is required")
+    @Positive(message = "Amount must be in positive")
+
+    private BigDecimal intialDeposit;
+}
